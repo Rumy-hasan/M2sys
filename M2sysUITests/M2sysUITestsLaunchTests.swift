4df39cd00d3cd6803a85d3562071a0ce9682f552
Rumy Hasan <Rumy-hasan@users.noreply.github.com>
2022-05-24 00:15:45 +0600

    Initial Commit

---
+++ M2sysUITests/M2sysUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  M2sysUITestsLaunchTests.swift
+//  M2sysUITests
+//
+//  Created by Paradox Space Rumy M1 on 24/5/22.
+//
+
+import XCTest
+
+class M2sysUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
